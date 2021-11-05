a8eda86adc0e99fb8e1f6e7129d8c272d3f3f8b3
Umar Niazi <uhkniazi@gmail.com>
2021-11-05 15:52:41 +0000

    added annotations to results dataframe

--- 09_annotateVCF.R
+++ 09_annotateVCF.R
@@ -7,6 +7,7 @@ source('header.R')
 
 library(VariantAnnotation)
 
+dfDeepSNV = read.csv('results/pilotDataVariantCalls.csv', header=T, row.names=1)
 oVcf = readVcf('results/pilotResults.vcf', genome = 'hg38')
 header(oVcf)
 ## header slots
@@ -37,6 +38,7 @@ library(SNPlocs.Hsapiens.dbSNP151.GRCh38)
 oDBSnp = SNPlocs.Hsapiens.dbSNP151.GRCh38
 
 oGRsnps = rowRanges(oVcf)
+#oGRsnps = as(oVcf, 'VRanges')
 seqnames(oGRsnps)
 seqlevels(oGRsnps)
 ## change the style to match snplocs package
@@ -48,9 +50,6 @@ seqlevels(oGRsnps)
 ## do some acrobatics with seqlevels to perform search
 ## as this will give error
 oGPsnps = snpsByOverlaps(oDBSnp, oGRsnps)
-# oGRsnps = GRanges(as.character(seqnames(oGRsnps)),
-#                   ranges(oGRsnps),
-#                   )
 
 seqlevels(oGRsnps) = seqlevels(oDBSnp)
 seqinfo(oGRsnps) = seqinfo(oDBSnp) 
@@ -61,6 +60,17 @@ oGPsnps = snpsByOverlaps(oDBSnp, oGRsnps)
 #IUPAC_CODE_MAP
 oGPsnps
 
+f = findOverlaps(oGRsnps, oGPsnps)
+oGRsnps$RefSNP_id = NA_character_
+oGRsnps$RefSNP_id[queryHits(f)] = oGPsnps$RefSNP_id[subjectHits(f)]
+oGRsnps.snploc = oGRsnps
+## match the names with deepSNV results table
+gr = GRanges(as.character(dfDeepSNV$chr), IRanges(start=dfDeepSNV$pos, width = 1))
+seqlevelsStyle(oGRsnps.snploc) = 'UCSC'
+i = findOverlaps(oGRsnps.snploc, gr)
+identical(queryHits(i), subjectHits(i))
+dfDeepSNV$RefSNP_id = oGRsnps.snploc$RefSNP_id
+
 ## location of the variants 
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 oTxdb = TxDb.Hsapiens.UCSC.hg38.knownGene
@@ -68,8 +78,22 @@ seqlevelsStyle(oTxdb)
 oGRsnps = rowRanges(oVcf)
 seqlevelsStyle(oGRsnps)
 seqlevelsStyle(oTxdb) = 'UCSC'
-oGRlocations = locateVariants(oGRsnps, oTxdb, AllVariants())
+genome(oGRsnps)
+genome(oTxdb)
 
+oGRlocations = locateVariants(oGRsnps, oTxdb, AllVariants())
+library(org.Hs.eg.db)
 ## summarize each snp by location
-lSnpLoc = split(oGRlocations$LOCATION, oGRlocations$QUERYID)
-sapply(lSnpLoc, function(x) table(x))
+dfSymbol = AnnotationDbi::select(org.Hs.eg.db, keys = oGRlocations$GENEID, columns = 'SYMBOL',
+                                keytype = 'ENTREZID')
+## check if names match with deepsnv results
+table(dfDeepSNV$ENTREZID %in% oGRlocations$GENEID)
+table(rownames(dfDeepSNV) %in% dfSymbol$SYMBOL)
+
+#lSnpLoc = split(oGRlocations$LOCATION, oGRlocations$QUERYID)
+lSnpLoc = split(oGRlocations$LOCATION, dfSymbol$SYMBOL)
+length(lSnpLoc)
+dfLocations = data.frame(sapply(lSnpLoc, function(x) table(x)))
+
+write.csv(dfDeepSNV, file='results/pilotDataVariantCalls_RefSNP.csv')
+write.csv(dfLocations, file='results/pilotDataVariantLocations.csv')
